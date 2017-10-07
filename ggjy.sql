cd54765e864ca1a4a9db55f303a9701434bc77df
hanqingzhu <zhuhanqing258@foxmail.com>
2017-10-07 17:41:58 +0800

    update the sql of the project ggjy

--- ggjy.sql
+++ ggjy.sql
@@ -1,4 +1,4 @@
-create database guigujingying;
+﻿create database guigujingying;
 create table menu(
        id int(2) not null primary key,
        name varchar(30) default null
@@ -32,3 +32,42 @@ CREATE TABLE `user` (
   `qq` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into menu(id,`name`) values (1,'guiGuZiXun');
+insert into menu(id,`name`) values (2,'gaoXinJiShu');
+insert into menu(id,`name`) values (3,'xiaoYuanGuiGu');
+insert into menu(id,`name`) values (4,'chuangYeXianFeng');
+insert into menu(id,`name`) values (5,'guanLiZhiDao');
+insert into menu(id,`name`) values (6,'chuangYePingTai');
+insert into menu(id,`name`) values (7,'chuangYeZiYuan');
+
+insert into submenu(id,`name`,parentId) values (1,'guiGuYaoWen',1);
+insert into submenu(id,`name`,parentId) values (1,'guiGuZhuanQu',1);
+insert into submenu(id,`name`,parentId) values (1,'IT',2);
+insert into submenu(id,`name`,parentId) values (1,'tongXin',2);
+insert into submenu(id,`name`,parentId) values (1,'qiChe',2);
+insert into submenu(id,`name`,parentId) values (1,'jiaDian',2);
+insert into submenu(id,`name`,parentId) values (1,'shengWuYiYao',2);
+insert into submenu(id,`name`,parentId) values (1,'xinCaiLiao',2);
+insert into submenu(id,`name`,parentId) values (1,'xinNengYuan',2);
+insert into submenu(id,`name`,parentId) values (1,'keJiChuangXin',3);
+insert into submenu(id,`name`,parentId) values (1,'xueZiJingJi',3);
+insert into submenu(id,`name`,parentId) values (1,'gaoXiaoPingTaiZhanShi',3);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeTuanDui',3);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeZhiXing',3);
+insert into submenu(id,`name`,parentId) values (1,'yeJieJingYing',4);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeXinXiu',4);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeXinDe',4);
+insert into submenu(id,`name`,parentId) values (1,'guanLiLiLun',5);
+insert into submenu(id,`name`,parentId) values (1,'qiYeZhanLue',5);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeGuanLi',5);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeWenHua',5);
+insert into submenu(id,`name`,parentId) values (1,'shuJiTuiJian',5);
+insert into submenu(id,`name`,parentId) values (1,'renCaiTuiJie',6);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeRongZi',6);
+insert into submenu(id,`name`,parentId) values (1,'jiShuTuiGuang',6);
+insert into submenu(id,`name`,parentId) values (1,'gaoXinYuanQu',7);
+insert into submenu(id,`name`,parentId) values (1,'fuHuaQi',7);
+insert into submenu(id,`name`,parentId) values (1,'teSeChanYeJiDi',7);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeJiJin',7);
+insert into submenu(id,`name`,parentId) values (1,'chuangYeZhengCe',7);
